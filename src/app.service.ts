1fe4860cdeafc53472f2580d84d58a5525d38478
KAISIN Guy <kaising@bpost.be>
2020-01-16 01:25:49 +0100

    add log4js (not working !), and test message

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+//import * as log4js from 'log4js';
+//const logger = log4js.getLogger('AppService');
+
 @Injectable()
 export class AppService {
   getHello(): string {
+    //logger.debug('sending app message');
     return 'Hello World!, This is Liwa site2 api talking...';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+//import * as log4js from 'log4js';
+
+//const cwd = process.cwd();
+// log4js.configure( cwd + '/config/log4js.json');
+//log4js.configure( './config/log4js.json');
+//const logger = log4js.getLogger('app');
+
+//logger.info('Starting liwa api server...');
+
 async function bootstrap() {
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  //logger.info('Liwa api server started: port=' + port);
   await app.listen(port);
 }
 bootstrap();
