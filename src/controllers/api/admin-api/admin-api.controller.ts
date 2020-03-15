3aa7244712fee91260a10e99641aa59beb05efe0
KAISIN Guy <kaising@bpost.be>
2020-03-15 22:14:30 +0100

    using only wawa10 db

--- src/controllers/api/admin-api/admin-api.controller.ts
+++ src/controllers/api/admin-api/admin-api.controller.ts
@@ -14,4 +14,5 @@ export class AdminApiController
     {
       return await this.soapService.getTeamList();
     }
+    
 }
--- src/modules/database/database.providers.ts
+++ src/modules/database/database.providers.ts
@@ -22,22 +22,24 @@ export const databaseProviders = [
       synchronize: false,
     }),
   },
+  /*,
   {
     name: 'wawa9',
     provide: 'wawa9_DbConnectionToken',
     useFactory: async () => createConnection({
-      type: 'mysql' /*configService.get('db_type')*/,      // this is an enumeration ! not possible to use this config value !
-      host: /*'localhost'*/ configService.get('wawa9_db_hostname'),
-      port: /*5432*/ +configService.get('wawa9_db_port'),
-      username: /*'chat'*/ configService.get('wawa9_db_username'),
-      password: /*'chat'*/ configService.get('wawa9_db_password'),
-      database: /*'chat'*/ configService.get('wawa9_db_name'),
+      type: 'mysql' /*configService.get('db_type')* /,      // this is an enumeration ! not possible to use this config value !
+      host: / *'localhost'* / configService.get('wawa9_db_hostname'),
+      port: /*5432* / +configService.get('wawa9_db_port'),
+      username: /*'chat'* / configService.get('wawa9_db_username'),
+      password: /*'chat'* / configService.get('wawa9_db_password'),
+      database: /*'chat'* / configService.get('wawa9_db_name'),
       logging: false, // In local if this value is set to true, the queries will be printed in orm.logs file
       logger: 'file', // in the folder where package.json file is located.
       entities: [
-        __dirname + '/../**/*.entity{.ts,.js}',
+        __dirname + '/../ * * /*.entity{.ts,.js}',
       ],
       synchronize: false,
     }),
   },
+  */
 ];
