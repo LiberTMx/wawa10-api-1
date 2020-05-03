2523cbba81b12a23c2ecf9ffee0d5c994b5a62f1
KAISIN Guy <kaising@bpost.be>
2020-05-03 16:38:21 +0200

    correct sql script

--- database/NEW/2020/05/196_rename_fonction_link_table.sql
+++ database/NEW/2020/05/196_rename_fonction_link_table.sql
@@ -6,7 +6,7 @@ DROP FOREIGN KEY auth_user_fonction_f_fk,
 DROP FOREIGN KEY auth_user_fonction_u_fk;
 ALTER TABLE auth_user_fonctions_auth_fonction
 CHANGE COLUMN auth_user_id authUserId INT(11) NOT NULL ,
-CHANGE COLUMN fonction_i authFonctionId INT(11) NOT NULL ;
+CHANGE COLUMN fonction_id authFonctionId INT(11) NOT NULL ;
 ALTER TABLE auth_user_fonctions_auth_fonction 
 ADD CONSTRAINT auth_user_fonction_f_fk
   FOREIGN KEY (authFonctionId)
